067cd0c42e4e608569e63df2359bdb08f188e41e
GitHub Action <action@github.com>
2021-01-24 18:31:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/blendthink/androidtemplates/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/blendthink/androidtemplates/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.blendthink.androidtemplates.listeners
 
+import com.github.blendthink.androidtemplates.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/blendthink/androidtemplates/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.blendthink.androidtemplates.services
+
+import com.github.blendthink.androidtemplates.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
